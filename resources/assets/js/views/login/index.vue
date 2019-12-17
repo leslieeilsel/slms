3fd649bef69b094b71f5c5d7b66bd502c5741892
Leslie Lau <1178273431@qq.com>
2019-12-17 14:49:23 +0800

    feat: 更换登录验证码

--- resources/assets/js/views/login/index.vue
+++ resources/assets/js/views/login/index.vue
@@ -1,43 +1,55 @@
 <template>
   <div id="background" class="content">
     <p class="user-layout-title">陕西体育彩票财务数据综合管理系统</p>
-    <Row type="flex" justify="center" class-name="row">
-      <br>
+    <Row type="flex" justify="center" class-name="row" @keydown.enter.native="submit('form')">
+      <br />
       <Col class="loginFrom">
-        <Tabs value="name1">
-          <TabPane label="账户密码登录" name="name1"></TabPane>
-        </Tabs>
         <Form ref="form" :model="form" :rules="ruleInline">
           <FormItem prop="email">
-            <Input type="text" size="large" v-model="form.email" :placeholder="$t('login.username')">
+            <Input
+              type="text"
+              size="large"
+              v-model="form.email"
+              :placeholder="$t('login.username')"
+            >
               <Icon type="md-person" slot="prepend"></Icon>
             </Input>
           </FormItem>
-          <FormItem prop="password" class="bottom">
-            <Input type="password" size="large" v-model="form.password" :placeholder="$t('login.password')">
+          <FormItem prop="password">
+            <Input
+              type="password"
+              size="large"
+              v-model="form.password"
+              :placeholder="$t('login.password')"
+            >
               <Icon type="md-lock" slot="prepend"></Icon>
             </Input>
           </FormItem>
+          <FormItem prop="verifyCode">
+            <Input
+              type="text"
+              size="large"
+              v-model="form.verifyCode"
+              placeholder="请输入验证码（不区分大小写）"
+              style="width: 238px;"
+            />
+            <vue-img-verify @getImgCode="getImgCode" ref="vueImgVerify" />
+          </FormItem>
           <FormItem class="bottom">
-            <Checkbox size="large" v-model="form.remember" class="rememberMe">记住我</Checkbox>
+            <Checkbox size="large" v-model="form.remember" class="rememberMe">自动登录</Checkbox>
             <router-link to="/password/send" class="forgetPassword">
               <span>忘记密码</span>
             </router-link>
           </FormItem>
-          <div ref="vaptcha" class="bottom" style="width: 368px;height: 40px">
-            <div class="vaptcha-init-main">
-              <div class="vaptcha-init-loading">
-                <a href="https://www.vaptcha.com/" target="_blank">
-                  <img src="https://cdn.vaptcha.com/vaptcha-loading.gif" alt=""/>
-                </a>
-                <span class="vaptcha-text">Vaptcha启动中...</span>
-              </div>
-            </div>
-          </div>
           <FormItem class="bottom">
-            <Button type="primary" @click="submit('form')" :disabled="disabled" id="loginButton"
-                    size="large" long :loading="loading">登录
-            </Button>
+            <Button
+              type="primary"
+              @click="submit('form')"
+              id="loginButton"
+              size="large"
+              long
+              :loading="loading"
+            >登录</Button>
           </FormItem>
         </Form>
       </Col>
@@ -46,131 +58,102 @@
 </template>
 
 <script>
-  import './login.css'
+import "./login.css";
+import vueImgVerify from "../../components/ImgVerify";
 
-  const extend = function (to, _from) {
-    for (const key in _from) {
-      to[key] = _from[key]
-    }
-    return to
-  };
-  export default {
-    props: {
-      type: {
-        type: String,
-        default: 'click'
-      },
-      scene: {
-        type: String,
-        default: ''
-      },
-      vpStyle: {
-        type: String,
-        default: 'light'
-      },
-      color: {
-        type: String,
-        color: '#2d8cf0'
-      },
-      lang: {
-        type: String,
-        default: 'zh-CN'
-      },
-    },
-    name: "index",
-    data() {
-      return {
-        disabled: true,
-        loading: false,
-        form: {
-          email: '',
-          password: '',
-          remember: false
-        },
-        vaptchaObj: {},
-        ruleInline: {
-          email: [
-            {required: true, message: '请填写用户名', trigger: 'blur'},
-          ],
-          password: [
-            {required: true, message: '请填写登录密码', trigger: 'blur'},
-          ]
-        },
-        basePath: window.document.location.hostname,
-      }
-    },
-    methods: {
-      loadV2Script() {
-        if (typeof window.vaptcha === 'function') { //如果已经加载就直接放回
-          return Promise.resolve();
-        } else {
-          return new Promise(resolve => {
-            let script = document.createElement('script');
-            script.src = 'https://cdn.vaptcha.com/v2.js';
-            script.async = true;
-            script.onload = script.onreadystatechange = function () {
-              if (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete') {
-                resolve();
-                script.onload = script.onreadystatechange = null;
-              }
-            };
-            document.getElementsByTagName("head")[0].appendChild(script);
-          })
-        }
-      },
-      submit(name) {
-        this.$refs[name].validate((valid) => {
-          if (valid) {
-            this.loading = true;
-            this.$store.dispatch('login', this.form).then((res) => {
-              this.vaptchaObj.reset(); //重置验证码
-              this.loading = false;
-              this.$router.push({name: 'home'});
-            });
-          }
-        })
-      }
-    },
-    mounted() {
-      let vid = this.basePath === 'localhost' ? '5bffa64bfc650eb8507698ed' : '5cf4e76afc650fc6543fada2';
-      let config = extend({
-        vid: vid,
-        container: this.$refs.vaptcha,
-        style: this.vpStyle
-      }, this.$props);
-      this.loadV2Script().then(() => {
-        window.vaptcha(config).then(obj => {
-          this.$emit('input', obj);
-          let _this = this;
-          _this.vaptchaObj = obj;
-          obj.listen('pass', function () {
-            _this.disabled = false;
-          });
-          obj.render();
-        })
-      })
-    }
+const extend = function(to, _from) {
+  for (const key in _from) {
+    to[key] = _from[key];
   }
+  return to;
+};
+export default {
+  name: "index",
+  components: {
+    vueImgVerify
+  },
+  data() {
+    const verifyCodeCheckValidate = (rule, value, callback) => {
+      let verifyCode = this.form.verifyCode.toLowerCase();
+      let imgCode = this.imgCode.toLowerCase();
+      if (verifyCode !== "") {
+        if (verifyCode !== imgCode) {
+          callback(new Error("验证码错误，请重新输入"));
+        } else {
+          callback();
+        }
+      } else {
+        callback(new Error("请输入验证码"));
+      }
+    };
+    return {
+      verifyCodeCheckValidate,
+      imgCode: "",
+      loading: false,
+      form: {
+        email: "",
+        password: "",
+        verifyCode: "",
+        remember: false
+      },
+      ruleInline: {
+        email: [{ required: true, message: "请填写用户名", trigger: "blur" }],
+        password: [
+          { required: true, message: "请填写登录密码", trigger: "blur" }
+        ],
+        verifyCode: [
+          {
+            required: true,
+            validator: verifyCodeCheckValidate,
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submit(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          this.loading = true;
+          this.$store.dispatch("login", this.form).then(res => {
+            this.loading = false;
+            this.$router.push({ name: "home" });
+          });
+        }
+      });
+    },
+    // 点击图片获取验证码
+    getImgCode(code) {
+      this.imgCode = code;
+      this.form.verifyCode = "";
+      // console.log('验证码: ' + this.imgCode);
+    }
+  },
+  mounted() {
+    this.imgCode = this.$refs.vueImgVerify.draw();
+  }
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  .loginFrom {
-    width: 368px;
-  }
-  
-  .row {
-    padding-top: 100px;
-    
-    .bottom {
-      margin-bottom: 14px;
-      
-      .rememberMe {
-        margin-left: 5px;
-      }
-      
-      .forgetPassword {
-        float: right;
-      }
+.loginFrom {
+  width: 368px;
+}
+
+.row {
+  padding-top: 160px;
+
+  .bottom {
+    margin-bottom: 14px;
+
+    .rememberMe {
+      margin-left: 5px;
+    }
+
+    .forgetPassword {
+      float: right;
     }
   }
+}
 </style>
